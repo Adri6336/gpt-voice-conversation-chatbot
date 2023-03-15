77d15c140890d3b79a6b18c149bbae7e813d1e15
self <self@xtb-3020.lan>
2023-03-14 21:39:24 -0500

    Created placeholder file for getting different prompts based on the model you are using.

---
+++ gpt_prompts.py
@@ -0,0 +1,19 @@
+
+def get_initial_prompt(model: str):
+    '''
+    Placeholder function. In time will be fleshed out so
+    that users can take advantage of different models' 
+    capabilites with more expansive prompts. GPT-3 models
+    have less token allowance than GPT-4, so their prompts 
+    are necessarily smaller. GPT-4 prompts will be more 
+    expansive.
+    '''
+    pass
+
+def get_memories(model: str):
+    '''
+    Placeholder function. Will work with memories
+    in a way that depends on the model. For example,
+    GPT-4 will have a larger memory text prompt.
+    '''
+    pass
